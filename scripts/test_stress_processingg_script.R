dcef4dc67a9019d0305c12ca7fbc0fdf24325abd
Alana Muller <amuller@email.arizona.edu>
2023-04-09 21:39:25 -0700

    started a new script, it's not done yet

---
+++ scripts/test_stress_processingg_script.R
@@ -0,0 +1,12 @@
+
+setwd("C:/Users/almul/OneDrive/Desktop/Alana/UA/HSCL/Stress Shortcuts/stress-shortcuts-collab/data/tmp")
+
+import_file <- "navStress_pilotAM_city1_navigation_23-03-21D_16.35.12T.log"
+
+my_data <- read.delim(import_file)
+
+head(my_data)
+
+
+
+
