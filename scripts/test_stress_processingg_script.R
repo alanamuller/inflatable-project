7721cac53c2cff862f85f9f2c7bdb9df557e3c67
Alana Muller <amuller@email.arizona.edu>
2023-04-17 17:29:40 -0700

    tried regex, didn't work yet

--- scripts/test_stress_processingg_script.R
+++ scripts/test_stress_processingg_script.R
@@ -1,12 +1,28 @@
+library(regexPipes)
+library(regexSelect)
 
-setwd("C:/Users/almul/OneDrive/Desktop/Alana/UA/HSCL/Stress Shortcuts/stress-shortcuts-collab/data/tmp")
+setwd("C:/Users/amuller/Desktop/Alana/UA/HSCL/Stress Shortcuts/stress-shortcuts-collab/data/tmp")
 
-import_file <- "navStress_pilotAM_city1_navigation_23-03-21D_16.35.12T.log"
+input_file <- "mini_test_log.txt"
 
-my_data <- read.delim(import_file)
-
-head(my_data)
+input_lines <- readLines(input_file)
 
 
+position_numbers <- lapply(input_lines, function(line) {
+  regmatches(line, regexpr(line, "(?<=Position \\(xyz\\):\\s)[-0-9.]+", perl = TRUE))
+})
 
+# Convert the resulting list to a data frame
+df <- data.frame(do.call("rbind", position_numbers))
+
+# Rename the columns
+names(df) <- c("X", "Y", "Z")
+
+# Convert the columns to numeric format
+df$X <- as.numeric(df$X)
+df$Y <- as.numeric(df$Y)
+df$Z <- as.numeric(df$Z)
+
+# Print the resulting data frame
+print(df)
 
