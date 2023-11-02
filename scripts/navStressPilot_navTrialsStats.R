de6a883621994579d449e4653f62948b19ae0a4d
Alana Muller <amuller@email.arizona.edu>
2023-11-02 10:29:02 -0700

    added graphs

--- scripts/navStressPilot_navTrialsStats.R
+++ scripts/navStressPilot_navTrialsStats.R
@@ -3,6 +3,7 @@ rm(list = ls())
 
 library(readxl)
 library(tidyverse)
+library(ggplot2)
 
 
 # Set the directory where the Excel files are stored
@@ -11,24 +12,26 @@ setwd("E:/Nav Stress Pilot Data")
 import_data <- read_excel("pilot_navTestTrials.xlsx", sheet = "Sheet 1")
 
 # Copy of data to play with
-data <- import_data
+df <- import_data
 
 # Fix structure of the data: columns 1:11 should be factors
-data <- data %>%
+df <- df %>%
   mutate_at(vars(1:11), as.factor)
 
 ### Delete the trials that have a duration less than 1 because those are mistakes
 # Count trials to be deleted
-count <- sum(data$Navigate_duration > 1)
+count <- sum(df$Navigate_duration < 1)
 
 # Delete those rows
-cd <- subset(data, Navigate_duration > 1) # cd stands for "clean data"
+cd <- subset(df, Navigate_duration > 1) # cd stands for "clean data"
 
 ### Make some histograms - see the spread of the data
-hist(data$Navigate_actualPath)
-hist(data$Navigate_optimalPath)
-
-
+hist(df$Navigate_actualPath)
+hist(df$Navigate_optimalPath)
+hist(df$Navigate_duration)
+hist(df$overlap_outer)
+hist(df$overlap_inner)
+hist(df$total_grids_trial)
 
 ### Make some graphs
 
@@ -36,13 +39,25 @@ hist(data$Navigate_optimalPath)
 nav_dur <- cd %>%
   group_by(subjectID, block, path_type) %>%
   summarise(
-    mean_nav_dur = mean(Navigate_duration, na.rm = TRUE)
+    mean_nav_dur = mean(Navigate_duration, na.rm = TRUE),
+    sd = sd(Navigate_duration, na.rm = TRUE)
   )
 
-# possible violin plot
-testpic <- ggplot(nav_dur, aes(x = block, y = mean_nav_dur)) + geom_violin(draw_quantiles = c(0.25, 0.50, 0.75)) + 
-  theme_classic() + stat_summary(fun = "mean", geom = "crossbar", color = "red")
-testpic
+# Plots
+p <- ggplot(nav_dur, aes(x = block, y = mean_nav_dur)) +
+  geom_bar()
+p
+
+ggplot(nav_dur, aes(x = subjectID, y = mean_nav_dur)) +
+  geom_boxplot() + 
+  geom_jitter(color="gray", size=1, width = 0.15) +
+  theme_classic() +
+  xlab("Subject Number") +
+  ylab("Mean Nav Duration (s)") + 
+  theme(axis.text = element_text(size = 14), axis.title = element_text(size = 16), legend.position = "none") +
+  scale_fill_brewer(palette = "Paired")
+
+
 
 # FIGURE FOR MANUSCRIPT
 landmark_ttest$next_to_landmark <- factor(landmark_ttest$next_to_landmark, levels = c("y", "n"))
