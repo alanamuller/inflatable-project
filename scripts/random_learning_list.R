ee49c4f7a25f908db3e015ef677839e1fc7d8365
Alana Muller <amuller@email.arizona.edu>
2023-11-20 18:13:28 -0700

    added script to randomize order of the learning trials

---
+++ scripts/random_learning_list.R
@@ -0,0 +1,12 @@
+# Randomize list of learning trials
+# Probably don't have double ones together though
+
+# make list
+list <- c("1", "1", "2", "2", "2", "2")
+
+# randomize list
+rando_list <- sample(list)
+
+print(rando_list)
+
+
