4da7b2f3ae9e8c6cc7264c01bf866f2bdfb02cac
Alana Muller <amuller@email.arizona.edu>
2024-02-05 18:07:49 -0700

    added inner and outer list makings

--- scripts/random_learning_list.R
+++ scripts/random_learning_list.R
@@ -1,8 +1,18 @@
 # Randomize list of learning trials
 # Probably don't have double ones together though
 
-# make list
-list <- c("1", "1", "2", "2", "2", "2")
+# make list when outer is more familiar
+list <- c("O", "O", "O", "O", "I", "I")
+
+# randomize list
+rando_list <- sample(list)
+
+print(rando_list)
+
+
+
+# make list when inner is more familiar
+list <- c("I", "I", "I", "I", "O", "O")
 
 # randomize list
 rando_list <- sample(list)
