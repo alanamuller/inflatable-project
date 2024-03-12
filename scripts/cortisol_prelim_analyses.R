44406b5ce48444f4267df477ebd28def7e01e426
Alana Muller <amuller@email.arizona.edu>
2024-03-11 17:05:22 -0700

    added more to the script, still needs so much work

--- scripts/cortisol_prelim_analyses.R
+++ scripts/cortisol_prelim_analyses.R
@@ -17,6 +17,15 @@ samples12Data <- readxl::read_excel("saliva_data_bySubject.xlsx", sheet = "12sam
 small_data <- samples9Data %>% 
   filter(subjNum >= 2 & subjNum <= 3)
 
+data_ctrl <- samples9Data %>%
+  filter(condition == "ctrl")
+
+data_cp <- samples9Data %>%
+  filter(condition == "cp")
+
+data_fire <- samples9Data %>%
+  filter(condition == "fire")
+
 summaryStats <- samples9Data%>%
   group_by(condition, time) %>%
   get_summary_stats(mean_cort, type = "mean_sd")
@@ -31,6 +40,11 @@ ggplot(data = samples9Data, aes(x=factor(time, level = level_order), y=mean_cort
   facet_wrap(vars(condition)) +
   labs(x = "Time", y = "Cortisol")
 
+ggplot(data = samples9Data, aes(x=factor(time, level = level_order), y=mean_cort)) +
+  geom_point() +
+  geom_line(aes(group = subjNum)) +
+  facet_grid(vars(condition)) +
+  labs(x = "Time", y = "Cortisol")
 
 
 ggplot(data = small_data, aes(x = factor(time, level = level_order), y = mean_cort, group = condition)) +
@@ -38,5 +52,16 @@ ggplot(data = small_data, aes(x = factor(time, level = level_order), y = mean_co
   geom_line(aes(color = condition)) +
   labs(x = "Time", y = "Cortisol")
 
+# Checking for outliers
+samples9Data %>%
+  group_by(time) %>%
+  identify_outliers(mean_cort)
 
+# Checking normality
+samples9Data %>%
+  group_by(time) %>%
+  shapiro_test(mean_cort)
 
+# quick and dirty anova
+res.aov <- anova_test(data = data_fire, dv = mean_cort, wid = subjNum, within = time)
+get_anova_table(res.aov)
