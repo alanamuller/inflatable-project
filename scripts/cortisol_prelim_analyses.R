6873087f4ab75b84c90d9c36a0959384cdd42485
Alana Muller <amuller@email.arizona.edu>
2024-08-27 16:20:14 -0700

    Changed the cortisol script to be more concise

--- scripts/cortisol_prelim_analyses.R
+++ scripts/cortisol_prelim_analyses.R
@@ -15,34 +15,68 @@ setwd("E:/Nav Stress Data/Salimetrics reports") # from hard drive
 samples9Data <- readxl::read_excel("saliva_data_bySubject.xlsx", sheet = "9samples")
 samples12Data <- readxl::read_excel("saliva_data_bySubject.xlsx", sheet = "12samples")
 
-samples9Data$cort_nmol_L <- samples9Data$mean_cort*276
-samples12Data$cort_nmol_L <- samples12Data$mean_cort*276
-
 all_data <- rbind(samples9Data, samples12Data)
+
+all_data$log_cort <- log(all_data$mean_cort) # calculate log cortisol
+all_data$cort_nmol_L <- all_data$mean_cort *276 # calculate mnol/L for cortisol
+
+
+# Make dataframe to add a column to correct for the baseline for that condition
+# Make a wide version of the data
+wide_data <- pivot_wider(data = all_data, id_cols = num_cond, names_from = time, values_from = mean_cort)
+
+# Add baseline corrected columns
+wide_data$bc_post1 <- wide_data$post1 - wide_data$pre
+wide_data$bc_post15 <- wide_data$post15 - wide_data$pre
+wide_data$bc_post30 <- wide_data$post30 - wide_data$pre
+
+# Make a long version of the data
+long_data <- pivot_longer(data = wide_data, cols = !num_cond,names_to = "time", values_to = "mean_cort")
+
+# Make separate dataframes for total mean cort and baseline corrected mean cort
+whole_cort <- long_data %>%
+  filter(time == "pre" | time == "post1" | time == "post15" | time == "post30")
+whole_cort <- whole_cort %>%
+  separate_wider_delim(num_cond, delim = "_", names = c("subjNum", "condition"))
+
+bc_cort <- long_data %>%
+  filter(time == "bc_post1" | time == "bc_post15" | time == "bc_post30")
+bc_cort <- bc_cort %>%
+  separate_wider_delim(num_cond, delim = "_", names = c("subjNum", "condition"))
 
 # Set the order of the x-axis
 level_order <- c('pre', 'post1', 'post15', 'post30')
+bc_level_order <- c('bc_post1', 'bc_post15', 'bc_post30')
 
 ##### Pics of all data
 
-# Plot with all participant separated by condition and time
-ggplot(data = all_data, aes(x=factor(time, level = level_order), y=cort_nmol_L)) +
+# Plot actual cortisol amount with all participant separated by condition and time
+ggplot(data = whole_cort, aes(x=factor(time, level = level_order), y=mean_cort)) +
   geom_boxplot(outliers = FALSE) +
   geom_jitter() +
   facet_wrap(vars(condition)) +
-  labs(x = "Time", y = "Cortisol (nmol/L)" )
+  labs(x = "Time", y = "Cortisol (µg/dL)" )
 
-ggplot(data = all_data, aes(x=factor(time, level = level_order), y=cort_nmol_L)) +
+# Line plot with Ss's amounts connected
+ggplot(data = whole_cort, aes(x=factor(time, level = level_order), y=mean_cort)) +
   geom_point() +
   geom_line(aes(group = subjNum)) +
   facet_grid(vars(condition)) +
-  labs(x = "Time", y = "Cortisol (nmol/L)" )
+  labs(x = "Time", y = "Cortisol (µg/dL)" )
 
-bxp <- ggboxplot(
-  all_data, x = "time", y = "cort_nmol_L", 
-  color = "condition"
-)
-bxp
+# Plot with baseline corrected cort
+ggplot(data = bc_cort, aes(x=factor(time, level = bc_level_order), y=mean_cort)) +
+  geom_boxplot(outliers = FALSE) +
+  geom_jitter() +
+  facet_wrap(vars(condition)) +
+  labs(x = "Time", y = "Cortisol (µg/dL)" )
+
+# Line plot with Ss's amounts connected
+ggplot(data = bc_cort, aes(x=factor(time, level = bc_level_order), y=mean_cort)) +
+  geom_point() +
+  geom_line(aes(group = subjNum)) +
+  facet_grid(vars(condition)) +
+  labs(x = "Time", y = "Cortisol (µg/dL)" )
 
 ##### Pics without 10, 15, 20, 23, 28 that didn't finish the cp task
 
@@ -50,77 +84,108 @@ bxp
 participants_to_exclude <- c(10, 15, 20, 23, 28)
 
 # Exclude data from the specified participants
-finished_cp <- all_data %>%
+whole_cort_cpSuccess <- whole_cort %>%
   filter(!subjNum %in% participants_to_exclude)
+whole_cort_cpSuccess$log_cort <- log(whole_cort_cpSuccess$mean_cort)
 
-# Plot with all participant separated by condition and time
-ggplot(data = finished_cp, aes(x=factor(time, level = level_order), y=cort_nmol_L)) +
+bc_cort_cpSuccess <- bc_cort %>%
+  filter(!subjNum %in% participants_to_exclude)
+bc_cort_cpSuccess$log_cort <- log(bc_cort_cpSuccess$mean_cort + 1) # plus 1 so NaNs aren't produced
+
+# Make a separate dataframe with only complete cases
+# Participants to exclude
+incompletes_to_exclude <- c(1, 2, 3, 5, 6, 8, 9, 16, 22, 30)
+
+# Exclude data from the specified participants
+whole_cort_complete <- whole_cort_cpSuccess %>%
+  filter(!subjNum %in% incompletes_to_exclude)
+
+# Exclude data from the specified participants
+bc_cort_complete <- bc_cort_cpSuccess %>%
+  filter(!subjNum %in% incompletes_to_exclude)
+
+
+# Plot actual cortisol amount with all participant separated by condition and time
+ggplot(data = all_data, aes(x=factor(time, level = level_order), y=mean_cort)) +
   geom_boxplot(outliers = FALSE) +
   geom_jitter() +
   facet_wrap(vars(condition)) +
-  labs(x = "Time", y = "Cortisol (nmol/L)" )
+  labs(x = "Time", y = "Cortisol (µg/dL)" )
+
+# Line plot with Ss's amounts connected
+ggplot(data = all_data, aes(x=factor(time, level = level_order), y=mean_cort)) +
+  geom_point() +
+  geom_line(aes(group = subjNum)) +
+  facet_grid(vars(condition)) +
+  labs(x = "Time", y = "Cortisol (µg/dL)" )
+
+# Plot with baseline corrected cort
+ggplot(data = bc_cort, aes(x=factor(time, level = bc_level_order), y=mean_cort)) +
+  geom_boxplot(outliers = FALSE) +
+  geom_jitter() +
+  facet_wrap(vars(condition)) +
+  labs(x = "Time", y = "Cortisol (µg/dL)" )
+
+# Line plot with Ss's amounts connected
+ggplot(data = bc_cort, aes(x=factor(time, level = bc_level_order), y=mean_cort)) +
+  geom_point() +
+  geom_line(aes(group = subjNum)) +
+  facet_grid(vars(condition)) +
+  labs(x = "Time", y = "Cortisol (µg/dL)" )
 
 ########## Checking assumptions
 
+##### There are outliers but I need all the data points for complete observations so no outliers are being deleted
+
 ##### All data
 
-# all_data outliers
-outliers_allData <- all_data %>%
-  group_by(time, condition) %>%
-  identify_outliers(cort_nmol_L)
-
-mean_cort <- mean(all_data$cort_nmol_L)
-sd_cort <- sd(all_data$cort_nmol_L)
-
-no_outliers_allData <- subset(all_data, all_data$cort_nmol_L > mean_cort - sd_cort*2.5 & all_data$cort_nmol_L < mean_cort + sd_cort*2.5)
-
-no_outliers_allData$log_cort <- log(no_outliers_allData$cort_nmol_L)
-
 # Checking normality
-ggqqplot(no_outliers_allData$cort_nmol_L)
-hist(no_outliers_allData$cort_nmol_L)
-hist(log(no_outliers_allData$cort_nmol_L))
+ggqqplot(whole_cort_cpSuccess$mean_cort)
+hist(whole_cort_cpSuccess$mean_cort)
+hist(whole_cort_cpSuccess$log_cort)
+ggqqplot(whole_cort_cpSuccess$log_cort)
 
-normality_allData <- no_outliers_allData %>%
+normality_allData <- all_data %>%
   group_by(time) %>%
   shapiro_test(log_cort)
+normality_allData
 
 # anova
-res.aov <- anova_test(data = no_outliers_allData, dv = log_cort, wid = subjNum, within = c(condition,time))
+res.aov <- anova_test(data = whole_cort_cpSuccess, dv = log_cort, wid = subjNum, within = c(condition,time))
 get_anova_table(res.aov)
 
 # testing simple main effects
 
 # testing effect of condition at every time point
-one.way <- no_outliers_allData %>%
+one.way <- whole_cort_cpSuccess %>%
   group_by(time) %>%
   anova_test(dv = log_cort, wid = subjNum, within = condition) %>%
   get_anova_table() %>%
   adjust_pvalue(method = "bonferroni")
 one.way
 
-# unequal cases so pairing it won't work
-pwc <- no_outliers_allData %>%
+# equal cases so pairing it will work
+pwc <- whole_cort_complete %>%
   group_by(time) %>%
   pairwise_t_test(
-    log_cort ~ condition, paired =  FALSE,
+    log_cort ~ condition, paired =  TRUE,
     p.adjust.method = "bonferroni"
   )
 pwc
 
 
-one.way2 <- no_outliers_allData %>%
+one.way2 <- whole_cort_cpSuccess %>%
   group_by(condition) %>%
   anova_test(dv = log_cort, wid = subjNum, within = time) %>%
   get_anova_table() %>%
   adjust_pvalue(method = "bonferroni")
 one.way2
 
-# unequal cases so pairing it won't work
-pwc2 <- no_outliers_allData %>%
+# equal cases so pairing it will work
+pwc2 <- whole_cort_complete %>%
   group_by(condition) %>%
   pairwise_t_test(
-    log_cort ~ time, paired = FALSE,
+    log_cort ~ time, paired = TRUE,
     p.adjust.method = "bonferroni"
   )
 pwc2
@@ -131,59 +196,22 @@ cond.labs <- c("Cold Pressor", "Control Condition", "Fire Environment")
 names(cond.labs) <- c("cp", "ctrl", "fire")
 
 # Plot with all participant separated by condition and time
-ggplot(data = no_outliers_allData, aes(x=factor(time, level = level_order), y=log_cort)) +
+ggplot(data = all_data, aes(x=factor(time, level = level_order), y=log_cort)) +
   geom_boxplot(outliers = FALSE) +
   geom_jitter() +
   facet_wrap(vars(condition), labeller = labeller(condition = cond.labs)) +
-  labs(x = "Time", y = " Log Cortisol (log nmol/L)" )
-
-
-##### Stats without the 5 that didn't finish cp task
-# outliers
-outliers_finished_cp <- finished_cp %>%
-  group_by(time, condition) %>%
-  identify_outliers(cort_nmol_L)
-
-mean_cort <- mean(finished_cp$cort_nmol_L)
-sd_cort <- sd(finished_cp$cort_nmol_L)
-
-no_outliers_finished_cp <- subset(finished_cp, finished_cp$cort_nmol_L > mean_cort - sd_cort*2.5 & finished_cp$cort_nmol_L < mean_cort + sd_cort*2.5)
-
-no_outliers_finished_cp$log_cort <- log(no_outliers_finished_cp$cort_nmol_L)
-
-# Checking normality
-ggqqplot(no_outliers_finished_cp$cort_nmol_L)
-hist(no_outliers_finished_cp$cort_nmol_L)
-hist(log(no_outliers_finished_cp$cort_nmol_L))
-
-normality_allData <- no_outliers_finished_cp %>%
-  group_by(time, condition) %>%
-  shapiro_test(log_cort) # only one sig value so looks pretty good
-
-# anova
-res.aov <- anova_test(data = no_outliers_finished_cp, dv = log_cort, wid = subjNum, within = c(condition,time))
-get_anova_table(res.aov) # sig time and condition/time interaction
-
-# testing simple main effect of time
-one.wayTime <- no_outliers_finished_cp %>%
-  group_by(time) %>%
-  anova_test(dv = log_cort, wid = subjNum, within = condition) %>%
-  get_anova_table() %>%
-  adjust_pvalue(method = "bonferroni")
-one.wayTime # noting survived adjusted p value
-
-
+  labs(x = "Time", y = " Log Cortisol" )
 
 # Make a plot
 cond.labs <- c("Cold Pressor", "Control", "Fire Environment")
 names(cond.labs) <- c("cp", "ctrl", "fire")
 
 # Plot with all participant separated by condition and time
-cort_graph1 <- ggplot(data = no_outliers_finished_cp, aes(x=factor(time, level = level_order), y=log_cort)) +
+cort_graph1 <- ggplot(data = whole_cort_complete, aes(x=factor(time, level = level_order), y=log_cort)) +
   geom_boxplot(outliers = FALSE) +
   geom_jitter() +
   facet_wrap(vars(condition), labeller = labeller(condition = cond.labs)) +
-  labs(x = "Time", y = " Log Cortisol (log nmol/L)" ) +
+  labs(x = "Time", y = " Log Cortisol" ) +
   theme(axis.text.x = element_text(size = 13), 
         axis.text.y = element_text(size = 17), 
         axis.title.x = element_text(size = 17),
@@ -194,137 +222,72 @@ cort_graph1 <- ggplot(data = no_outliers_finished_cp, aes(x=factor(time, level =
   scale_x_discrete(labels = c("Pre", "Post1", "Post15", "Post30"))
 
 
-#jpeg("C:/Users/amuller/Desktop/Alana/UA/HSCL/Conferences/iNAV 2024/log_cortisol1.jpeg", width = 9, height = 5.75, units = 'in', res = 500)
+#jpeg("C:/Users/amuller/Desktwhole_cort_complete#jpeg("C:/Users/amuller/Desktop/Alana/UA/HSCL/Conferences/iNAV 2024/log_cortisol1.jpeg", width = 9, height = 5.75, units = 'in', res = 500)
 cort_graph1
 #dev.off()
 
-
-
-##### Stats with only cases that had 12 samples - aka complete cases
-# exclude participant 16, 17, 20, 22, and 23 for now - some incomplete and some waiting on Salimetrics analysis
-
-# Participants to exclude
-participants_to_exclude <- c(16, 17, 20, 22, 23)
-
-# Exclude data from the specified participants
-completeSamples12 <- samples12Data %>%
-  filter(!subjNum %in% participants_to_exclude)
+########## Stats with baseline corrected cortisol ##########
 
 # Checking normality
-ggqqplot(completeSamples12$cort_nmol_L)
-hist(completeSamples12$cort_nmol_L)
-hist(log(completeSamples12$cort_nmol_L))
+ggqqplot(bc_cort_complete$mean_cort)
+hist(bc_cort_complete$mean_cort)
+hist(bc_cort_complete$log_cort)
+ggqqplot(bc_cort_complete$log_cort)
 
-completeSamples12$log_cort <- log(completeSamples12$cort_nmol_L)
-
-normality_12samples <- completeSamples12 %>%
+normality_bc_cort <- bc_cort_complete %>%
   group_by(time) %>%
-  shapiro_test(log_cort) # normal!
+  shapiro_test(log_cort)
+normality_bc_cort
 
-res.aov <- anova_test(data = completeSamples12, dv = log_cort, wid = subjNum, within = c(condition,time))
-get_anova_table(res.aov) # sig interaction
+# anova
+res.aov <- anova_test(data = bc_cort_complete, dv = log_cort, wid = subjNum, within = c(condition,time))
+get_anova_table(res.aov)
+
+# testing simple main effects
 
 # testing effect of condition at every time point
-one.way3 <- completeSamples12 %>%
+one.way <- bc_cort_complete %>%
   group_by(time) %>%
   anova_test(dv = log_cort, wid = subjNum, within = condition) %>%
   get_anova_table() %>%
   adjust_pvalue(method = "bonferroni")
-one.way3 # nothing survived adjusted p
+one.way
 
-# check pairwise comparisons
-pwc3 <- completeSamples12 %>%
+# equal cases so pairing it will work
+pwc <- bc_cort_complete %>%
   group_by(time) %>%
   pairwise_t_test(
     log_cort ~ condition, paired =  TRUE,
     p.adjust.method = "bonferroni"
   )
-pwc3 # nothing survived adjusted p
+pwc
 
-one.way4 <- completeSamples12 %>%
+
+one.way2 <- bc_cort_complete %>%
   group_by(condition) %>%
   anova_test(dv = log_cort, wid = subjNum, within = time) %>%
   get_anova_table() %>%
   adjust_pvalue(method = "bonferroni")
-one.way4 # cp and ctrl sig, survived adjusted p
+one.way2
 
-# pairwise comparisons
-pwc4 <- completeSamples12 %>%
+# equal cases so pairing it will work
+pwc2 <- bc_cort_complete %>%
   group_by(condition) %>%
   pairwise_t_test(
     log_cort ~ time, paired = TRUE,
     p.adjust.method = "bonferroni"
   )
-pwc4 # 3 comparisons sig
-
-# plot for iNAV
-# Make a plot
-cond.labs <- c("Cold Pressor", "Control", "Fire Environment")
-names(cond.labs) <- c("cp", "ctrl", "fire")
-
-# Plot with all participant separated by condition and time
-cort_graph <- ggplot(data = completeSamples12, aes(x=factor(time, level = level_order), y=log_cort)) +
-  geom_boxplot(outliers = FALSE, coef = 0) +
-  geom_point() +
-  geom_line(aes(group = subjNum)) +
-  facet_wrap(vars(condition), labeller = labeller(condition = cond.labs)) +
-  labs(x = "Time", y = " Log Cortisol (log nmol/L)" ) +
-  theme(axis.text.x = element_text(size = 13), 
-        axis.text.y = element_text(size = 17), 
-        axis.title.x = element_text(size = 17),
-        axis.title.y = element_text(size = 17),
-        legend.text = element_text(size = 17),
-        legend.title = element_text(size = 17), 
-        strip.text = element_text(size = 13)) +
-  scale_x_discrete(labels = c("Pre", "Post1", "Post15", "Post30"))
+pwc2
 
 
-jpeg("C:/Users/amuller/Desktop/Alana/UA/HSCL/Conferences/iNAV 2024/pics/log_cortisol.jpeg", width = 9, height = 5.75, units = 'in', res = 500)
-cort_graph
-dev.off()
 
 
-##### Correct using baseline so subtract pre from all posts
-smallData <- all_data[,c(3,6,9,11)]
-spreadData <- spread(smallData, condition_time, mean_cort)
-spreadData <- na.omit(spreadData)
 
-spreadData$cp_preMinusPre <- spreadData$cp_pre - spreadData$cp_pre
-spreadData$cp_post1MinusPre <- spreadData$cp_post1 - spreadData$cp_pre
-spreadData$cp_post15MinusPre <- spreadData$cp_post15 - spreadData$cp_pre
-spreadData$cp_post30MinusPre <- spreadData$cp_post30 - spreadData$cp_pre
 
-spreadData$ctrl_preMinusPre <- spreadData$ctrl_pre - spreadData$ctrl_pre
-spreadData$ctrl_post1MinusPre <- spreadData$ctrl_post1 - spreadData$ctrl_pre
-spreadData$ctrl_post15MinusPre <- spreadData$ctrl_post15 - spreadData$ctrl_pre
-spreadData$ctrl_post30MinusPre <- spreadData$ctrl_post30 - spreadData$ctrl_pre
 
-spreadData$fire_preMinusPre <- spreadData$fire_pre - spreadData$fire_pre
-spreadData$fire_post1MinusPre <- spreadData$fire_post1 - spreadData$fire_pre
-spreadData$fire_post15MinusPre <- spreadData$fire_post15 - spreadData$fire_pre
-spreadData$fire_post30MinusPre <- spreadData$fire_post30 - spreadData$fire_pre
-
-# name the columns you want to gather, the other columns will remain there
-gathered_columns <- c("cp_post1MinusPre", "cp_post15MinusPre", "cp_post30MinusPre", "ctrl_post1MinusPre", "ctrl_post15MinusPre", "ctrl_post30MinusPre", "fire_post1MinusPre", "fire_post15MinusPre", "fire_post30MinusPre")
-
-# gather the data for grid numbers
-baselineCorr_data <- gather(spreadData, key = cort_type, value = mean_cort_change, gathered_columns, factor_key = TRUE)
-
-baselineCorr_data <- baselineCorr_data %>%
-  separate(cort_type, into = c("condition", "time"), sep = "_")
-
-# Set the order of the x-axis
-level_order <- c('post1MinusPre', 'post15MinusPre', 'post30MinusPre')
-
-# Plot cort separated by condition and time
-ggplot(data = baselineCorr_data, aes(x=factor(time, level = level_order), y=mean_cort_change)) +
-  geom_boxplot(outliers = FALSE) +
-  geom_jitter() +
-  facet_wrap(vars(condition)) +
-  labs(x = "Time", y = "Cortisol (nmol/L)" )
 
 # Checking normality
-ggqqplot(baselineCorr_data$mean_cort_change)
+ggqqplot(bc_cort$mean_cort_change)
 hist(baselineCorr_data$mean_cort_change)
 hist(log(baselineCorr_data$mean_cort_change))
 
